43b03577b18732e55c0b6012d8da16f025bc8f05
unknown <jta@WK420.datasia.com>
2015-03-25 18:39:45 +0100

    input detection and conversion, 1/18

--- dist/vntype.js
+++ dist/vntype.js
@@ -17,12 +17,36 @@ vntype.run = function(){
 	// TMP
 	vntype.inputs[0] = vntype.input.select("test");
 
+	// é è & ' ~ (
+	/*vntype.diacritics = ['&#233;','&#232;','&amp;','&#39;','&#126;','&#40;'];*/
+	vntype.diacritics = ['é','è','&amp;','&#39;','&#126;','&#40;'];
+
 	window.setInterval(function() {
-		console.dir(vntype.inputs[0][0].value);
+
+		vntype.arr = vntype.inputs[0][0].value.split('');
+
+		if (vntype.arr instanceof Array) {
+
+			for (var i=0, l=vntype.arr.length; i<l; i++) {
+
+				if (vntype.arr[i] == vntype.diacritics[0]) {
+
+					if (vntype.arr[i-1] == 'a' ) {
+						vntype.arr.splice(i-1, 2, 'á');
+					}
+				}
+			}
+		}
+
+		vntype.str = vntype.arr.toString();
+
+		vntype.input.select("test").value = vntype.str;
+		console.dir(vntype.str);
+
 	}, 2000);
 };
 
-
+// ----============== POO after ==================---------------
 // ---- config class ----
 // MAYBE : separate classes in different files, using require.js
 
